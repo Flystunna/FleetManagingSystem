41ede568de1e30d5c6edf353b3d3281436812f90
flystunna <john.dhara@outlook.com>
2019-12-21 08:46:07 +0100

    dadd

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
-const path = require('path');
-const bodyparser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const flash = require('connect-flash');
-const session = require('express-session');
-const passport = require('passport');
-const mongo = require('mongodb');
-const mongoose = require('mongoose');
-const nodemailer = require('nodemailer');
-const passportLocalMongoose = require('passport-local-mongoose');
-const LocalStrategy = require('passport-local').Strategy;
-const async= require('async');
-const Swal = require('sweetalert2')
-require('dotenv').config()
-
-
+const path = require("path");
+const bodyparser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
+const mongo = require("mongodb");
+const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+const passportLocalMongoose = require("passport-local-mongoose");
+const LocalStrategy = require("passport-local").Strategy;
+const async = require("async");
+const Swal = require("sweetalert2");
+require("dotenv").config();
 
 // mongoose.connect('mongodb://localhost:27017/fleet', { useNewUrlParser: true }, (err) => {
 //     if (!err) { console.log('MongoDB Connection Succeeded.') }
@@ -25,64 +23,71 @@ require('dotenv').config()
 
 /// database connection
 var db = mongoose.connection;
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://flystunna:Flystunna1.@datas-mbypu.mongodb.net/admin?retryWrites=true&w=majority";
-const client = new MongoClient(uri,  { useNewUrlParser: true },{dbName: 'Users'});
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://flystunna:Flystunna1.@datas-mbypu.mongodb.net/admin?retryWrites=true&w=majority";
+const client = new MongoClient(
+  uri,
+  { useNewUrlParser: true },
+  { dbName: "Users" }
+);
 client.connect(err => {
-   collection = client.db("Users").collection("Users");
+  collection = client.db("Users").collection("Users");
   // perform actions on the collection object
-  if (!err) { console.log('MongoDB Connection is Succeeded.') }
-    else { console.log('Error in DB connection : ' + err) }
+  if (!err) {
+    console.log("MongoDB Connection is Succeeded.");
+  } else {
+    console.log("Error in DB connection : " + err);
+  }
 });
 //passport
 
-require('./config/passport')(passport);
-app.use(express.static(path.join(__dirname, 'public')));
-
+require("./config/passport")(passport);
+app.use(express.static(path.join(__dirname, "public")));
 
 //bodyparser
-app.use(bodyparser.urlencoded({ extended: false}));
+app.use(bodyparser.urlencoded({ extended: false }));
 
 //Express Session
-app.use(session({
-	secret: 'secret',
-	resave: true,
-	saveUninitialized: true,
-}));
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 //EJS
 app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //Connect Flash
 app.use(flash());
 
-
 //Global
 app.use((req, res, next) => {
-	res.locals.success_msg = req.flash('success_msg');
-	res.locals.error_msg = req.flash('error_msg');
-	res.locals.error = req.flash('error');
-  res.locals.msg = req.flash('msg');
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  res.locals.msg = req.flash("msg");
   res.locals.client = client;
   res.locals.currentUser = req.user;
-	next();
+  next();
 });
 
 app.locals.client = client;
 app.locals.uri = uri;
 
 //routes
-app.use('/', require('./routes/index'));
-app.use('/', require('./routes/users'));
+app.use("/", require("./routes/index"));
+app.use("/", require("./routes/users"));
 
-
-app.get('/', (req, res) => {
-  res.render('contact');
+app.get("/", (req, res) => {
+  res.render("contact");
 });
 
-app.post('/send', (req, res) => { 
+app.post("/send", (req, res) => {
   const output = `
     <p>You have a new contact request</p>
     <h3>Contact Details</h3>
@@ -96,45 +101,41 @@ app.post('/send', (req, res) => {
     <p>${req.body.message}</p>
   `;
 
- 
-
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    service: "Gmail",
     auth: {
-        user: 'process.env.DB_USER', 
-        pass: 'process.env.DB_PASS'  
-   },
-    tls:{
-      rejectUnauthorized:false
+      user: "flystunna1@gmail.com",
+      pass: "Stunna6882"
     }
   });
 
-
   // setup email data with unicode symbols
   let mailOptions = {
-      from: '"Nodemailer Contact" <your@email.com>', // sender address
-      to: 'John.dhara@gmail.com', // list of receivers
-      subject: 'Node Contact Request', // Subject line
-      text: 'Hello world?', // plain text body
-      html: output // html body
+    from: "flystunna1@gmail.com",
+    to: "req.body.email", // list of receivers
+    subject: "Node Contact Request", // Subject line
+    text: "Hello world?", // plain text body
+    html: output // html body
   };
 
   // send mail with defined transport object
   transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
-      }
-      console.log('Message sent: %s', info.messageId);   
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-      req.flash('msg', 'Email has been sent');
-      res.render('contact', {msg:'Email has been sent'});
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    req.flash("msg", "Email has been sent");
+    res.render("contact", { msg: "Email has been sent" });
   });
-  });
-
-app.listen(5000, () => {
-
-	console.log('Express server started at port : 5000');
 });
+
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("Express is working on port " + port);
+});
+
+// app.listen(8080, () => {
+//   console.log("Express server started at port : 5000");
+// });
